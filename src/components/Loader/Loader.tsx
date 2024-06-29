f08ddb37a9ea5e497163ff0b11ec7e6637932a9e
Anna <147976531+AnnaKhailo@users.noreply.github.com>
2024-06-29 20:59:52 +0300

    Update Loader.tsx

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,17 +1,15 @@
 import css from "./Loader.module.css";
-import { RotatingLines } from "react-loader-spinner";
+import { ThreeCircles } from "react-loader-spinner";
 
 const Loader: React.FC = () => {
   return (
     <div className={css.loader}>
-      <RotatingLines
+      <ThreeCircles
         visible={true}
-        height="48"
-        width="48"
-        color="grey"
-        strokeWidth="5"
-        animationDuration="0.75"
-        ariaLabel="rotating-lines-loading"
+        height="60"
+        width="60"
+        color="#264fbe"
+        ariaLabel="three-circles-loading"
         wrapperStyle={{}}
         wrapperClass=""
       />
